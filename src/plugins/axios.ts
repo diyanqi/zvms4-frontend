bf4f3a76c94c91da5bf6c25ac39d82855c227000
Dignite <60802943+diyanqi@users.noreply.github.com>
2025-08-17 11:23:57 +0800

    feat: implement dynamic base URL selection for API requests

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,19 +1,64 @@
 import axios, { AxiosError } from 'axios'
-import { ElMessageBox, ElNotification } from 'element-plus'
+import { ElMessage } from 'element-plus'
 import nprogress from 'nprogress'
 import 'nprogress/nprogress.css'
 import { parseJwt } from './jwt'
+import router from '@/router'
+import { useUserStore } from '@/stores/user'
 
-export const baseURL = import.meta.env.PROD ? 'https://api.zvms.site/api/' : 'http://localhost:8000/api/'
+// Function to get the value of a specific cookie
+function getCookieValue(cookieName: string) {
+  const cookies = document.cookie.split('; ')
+  for (const cookie of cookies) {
+    const [name, value] = cookie.split('=')
+    if (name === cookieName) {
+      return value
+    }
+  }
+  return null
+}
+
+function determineFastestBaseURL(urls: string[]): Promise<string> {
+  return new Promise((resolve) => {
+    let fastestURL = urls[0]
+    let fastestTime = Infinity
+
+    urls.forEach((url) => {
+      const startTime = performance.now()
+      fetch(url, { method: 'HEAD' })
+        .then(() => {
+          const elapsedTime = performance.now() - startTime
+          if (elapsedTime < fastestTime) {
+            fastestTime = elapsedTime
+            fastestURL = url
+          }
+        })
+        .catch(() => {})
+        .finally(() => {
+          if (urls.indexOf(url) === urls.length - 1) {
+            resolve(fastestURL)
+          }
+        })
+    })
+  })
+}
+
+const baseURLs = [
+  'https://api.zvms.site/api/',
+  'https://zvms-api.amzcd.top/api/'
+]
+
+export const baseURL = import.meta.env.PROD
+  ? await determineFastestBaseURL(baseURLs)
+  : 'http://localhost:8000/api/'
 
 const axiosInstance = axios.create({
   baseURL,
-  // If using `http`, it must be testing or the `window.crypto.subtle` will not work.
-  withCredentials: false,
+  withCredentials: true,
   timeout: 24000,
   headers: {
     'Content-type': 'application/json',
-    Authorization: localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : ''
+    'Clarity-ID': getCookieValue('_clck')?.split('%7C')[0] ?? ''
   }
 })
 
@@ -22,6 +67,10 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     nprogress.start()
+    const token = localStorage.getItem('token')
+    if (token && !config.headers['Authorization']) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
     return config
   },
   (error) => {
@@ -39,38 +88,69 @@ axiosInstance.interceptors.response.use(
   },
   (error: Error | AxiosError) => {
     nprogress.done()
+    let errorDisplayed = false
     if (axios.isAxiosError(error)) {
       if (error.response?.status === 401) {
         const token = parseJwt(localStorage.getItem('token') as string)
         if (token.payload.scope === 'access_token') {
-          ElNotification({
-            title: 'Error',
-            message: 'Your session has expired, or you are not authorized. Please login again.',
-            type: 'error'
-          })
-          localStorage.removeItem('token')
-          localStorage.removeItem('user')
-          window.location.href = '/user/login'
+          if (!errorDisplayed) {
+            errorDisplayed = true
+            ElMessage({
+              message: 'Session expired',
+              type: 'error',
+              grouping: true,
+              plain: true
+            })
+            localStorage.removeItem('token')
+            useUserStore()
+              .removeUser()
+              .then(() => {
+                router.push('/user/login')
+              })
+          }
         } else {
-          ElNotification({
-            title: 'Error',
+          ElMessage({
             message: 'Your password is wrong. Please operate again.',
-            type: 'error'
-          })
+            type: 'error',
+            grouping: true,
+            plain: true
+          }).then((r) => Promise.reject(r))
         }
       } else {
-        ElMessageBox({
-          title: 'Error',
-          message: 'data: ' + JSON.stringify(error.response?.data),
-          type: 'error'
-        }).then(() => Promise.reject(error))
+        if (!errorDisplayed) {
+          errorDisplayed = true
+          ElMessage({
+            message: error.response?.data?.detail
+              ? 'Error: ' +
+                (typeof error.response?.data?.detail === 'string'
+                  ? error.response?.data?.detail
+                  : JSON.stringify(error.response?.data?.detail))
+              : 'Something went wrong',
+            type: 'error',
+            grouping: true,
+            plain: true
+          })
+          if (!error.response?.data?.detail && navigator.onLine) {
+            router.push('/sww')
+          }
+        }
       }
     } else {
-      ElMessageBox({
-        title: 'Error',
-        message: 'error message: ' + error.message,
-        type: 'error'
-      }).then(() => Promise.reject(error))
+      if (!errorDisplayed) {
+        errorDisplayed = true
+        ElMessage({
+          message:
+            'Error:' + error.message
+              ? 'data: ' + JSON.stringify(error.message)
+              : 'Something went wrong',
+          type: 'error',
+          grouping: true,
+          plain: true
+        })
+        if (!error.message && navigator.onLine) {
+          router.push('/sww')
+        }
+      }
     }
   }
 )
